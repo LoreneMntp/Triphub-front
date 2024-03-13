e5c3e828445b4cee80c887cb21c616bce959c9d5
CAPPELLE Aymeric <cappelleaymeric@proton.me>
2024-03-13 15:25:31 +0100

    change default bg

--- screens/Stack/ShowActivityScreen.js
+++ screens/Stack/ShowActivityScreen.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import * as Notifications from "expo-notifications";
 import moment from "moment";
 import "moment/locale/fr";
-moment.locale('fr');
+moment.locale("fr");
 import { SquarePen, MapPin } from "lucide-react-native";
 import NetInfo from "@react-native-community/netinfo";
 import { useEffect, useState } from "react";
@@ -12,21 +12,26 @@ import { selectDay } from "../../reducers/users";
 export default function ShowActivityScreen({ navigation }) {
   const activity = useSelector((state) => state.user.value.selectedActivity);
   const selectedDay = useSelector((state) => state.user.value.selectedDay.day);
-  const selectedDate = useSelector((state) => state.user.value.selectedDay.date);
+  const selectedDate = useSelector(
+    (state) => state.user.value.selectedDay.date
+  );
 
   const [isConnected, setIsConnected] = useState(null);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = NetInfo.addEventListener(state => {
+    const unsubscribe = NetInfo.addEventListener((state) => {
       setIsConnected(state.isConnected);
     });
     return () => unsubscribe();
   }, []);
 
   const addNotification = async (minBeforeActivity) => {
-    const notificationTime = moment(activity.content.plannedAt).subtract(minBeforeActivity, "minutes");
+    const notificationTime = moment(activity.content.plannedAt).subtract(
+      minBeforeActivity,
+      "minutes"
+    );
 
     if (moment().isAfter(notificationTime)) {
       alert("L'heure de l'activité est déjà passée");
@@ -41,15 +46,29 @@ export default function ShowActivityScreen({ navigation }) {
     await Notifications.scheduleNotificationAsync({
       content: {
         title: "Rappel",
-        body: `Rappel pour l'activité ${activity.content.title} à ${moment(activity.content.plannedAt).format("LT")}`,
+        body: `Rappel pour l'activité ${activity.content.title} à ${moment(
+          activity.content.plannedAt
+        ).format("LT")}`,
       },
       trigger: notificationTime.toDate(),
     });
   };
 
   const notes = activity.content.notes.map((note, index) => (
-    <View key={index} style={{ backgroundColor: '#EEC170', padding: 10, borderRadius: 15, marginTop: 10, shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.25, shadowRadius: 3.84 }}>
-      <Text style={{ fontStyle: 'italic', fontSize: 15 }}>• {note}</Text>
+    <View
+      key={index}
+      style={{
+        backgroundColor: "#EEC170",
+        padding: 10,
+        borderRadius: 15,
+        marginTop: 10,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+      }}
+    >
+      <Text style={{ fontStyle: "italic", fontSize: 15 }}>• {note}</Text>
     </View>
   ));
 
@@ -59,54 +78,165 @@ export default function ShowActivityScreen({ navigation }) {
   };
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#F2F4F5' }}>
-      <ScrollView contentContainerStyle={{ paddingHorizontal: 10, alignItems: 'center', paddingTop: 30 }}>
-        <View style={{
-          width: '90%', backgroundColor: '#fff', borderRadius: 20, padding: 20, marginBottom: 20, elevation: 5, shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.25, shadowRadius: 3.84
-        }}>
-          {isConnected ? <Pressable onPress={handleEditActivity} disabled={!isConnected} style={{
-            alignSelf: 'flex-end', padding: 10, borderRadius: 25, backgroundColor: isConnected ? '#F2A65A' : '#BABABA', shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.25, shadowRadius: 3.84
-          }}>
-            <SquarePen size={20} color={isConnected ? "#FFF" : "#595959"} strokeWidth={3} />
-          </Pressable>: <Pressable
-          className="absolute top-4 right-4 p-2 rounded-full bg-[#BABABA]"
-          onPress={() => handleEditActivity()}
-          disabled={!isConnected}
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#F2F4F5" }}>
+      <ScrollView
+        contentContainerStyle={{
+          paddingHorizontal: 10,
+          alignItems: "center",
+          paddingTop: 30,
+        }}
+      >
+        <View
+          style={{
+            width: "90%",
+            backgroundColor: "#fff",
+            borderRadius: 20,
+            padding: 20,
+            marginBottom: 20,
+            elevation: 5,
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: 2 },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+          }}
         >
-          <SquarePen size={20} color="#595959" strokeWidth={3} />
-        </Pressable>}
-          <Text style={{ fontSize: 24, fontWeight: 'bold', textAlign: 'center', marginTop: 24 }}>
+          {isConnected ? (
+            <Pressable
+              onPress={handleEditActivity}
+              disabled={!isConnected}
+              style={{
+                alignSelf: "flex-end",
+                padding: 10,
+                borderRadius: 25,
+                backgroundColor: isConnected ? "#F2A65A" : "#BABABA",
+                shadowColor: "#000",
+                shadowOffset: { width: 0, height: 2 },
+                shadowOpacity: 0.25,
+                shadowRadius: 3.84,
+              }}
+            >
+              <SquarePen
+                size={20}
+                color={isConnected ? "#FFF" : "#595959"}
+                strokeWidth={3}
+              />
+            </Pressable>
+          ) : (
+            <Pressable
+              className="absolute top-4 right-4 p-2 rounded-full bg-[#BABABA]"
+              onPress={() => handleEditActivity()}
+              disabled={!isConnected}
+            >
+              <SquarePen size={20} color="#595959" strokeWidth={3} />
+            </Pressable>
+          )}
+          <Text
+            style={{
+              fontSize: 24,
+              fontWeight: "bold",
+              textAlign: "center",
+              marginTop: 24,
+            }}
+          >
             Votre Aventure
           </Text>
-          <Text style={{ fontSize: 40, fontWeight: 'bold', textAlign: 'center', marginTop: 16 }}>
+          <Text
+            style={{
+              fontSize: 40,
+              fontWeight: "bold",
+              textAlign: "center",
+              marginTop: 16,
+            }}
+          >
             {activity.content.title}
           </Text>
-          <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 16 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: 16,
+            }}
+          >
             <MapPin size={30} color="#000" />
-            <Text style={{ fontSize: 20, marginLeft: 8 }}>{activity.content.address}</Text>
+            <Text style={{ fontSize: 20, marginLeft: 8 }}>
+              {activity.content.address}
+            </Text>
           </View>
           {activity.content.notes.length > 0 ? (
-            <View style={{ paddingHorizontal: 10, marginTop: 16, backgroundColor: '#EEC170', padding: 15, borderRadius: 15 }}>
+            <View
+              style={{
+                paddingHorizontal: 10,
+                marginTop: 16,
+                backgroundColor: "#EEC170",
+                padding: 15,
+                borderRadius: 15,
+              }}
+            >
               {notes}
             </View>
           ) : (
-            <Text style={{ fontSize: 16, textAlign: 'center', marginTop: 20, fontStyle: 'italic' }}>
+            <Text
+              style={{
+                fontSize: 16,
+                textAlign: "center",
+                marginTop: 20,
+                fontStyle: "italic",
+              }}
+            >
               Aucune note pour cette aventure.
             </Text>
           )}
-          <View style={{ marginTop: 20, backgroundColor: '#EEC170', borderRadius: 15, padding: 20, shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.25, shadowRadius: 3.84 }}>
-            <Text style={{ marginBottom: 16, fontWeight: 'bold' }}>
+          <View
+            style={{
+              marginTop: 20,
+              backgroundColor: "#EEC170",
+              borderRadius: 15,
+              padding: 20,
+              shadowColor: "#000",
+              shadowOffset: { width: 0, height: 2 },
+              shadowOpacity: 0.25,
+              shadowRadius: 3.84,
+            }}
+          >
+            <Text style={{ marginBottom: 16, fontWeight: "bold" }}>
               Ajouter un rappel pour l'aventure :
             </Text>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-              <Pressable onPress={() => addNotification(5)} style={{ backgroundColor: '#F58549', paddingVertical: 10, paddingHorizontal: 20, borderRadius: 10 }}>
-                <Text style={{ color: '#FFF', fontWeight: '500' }}>5 min</Text>
+            <View
+              style={{ flexDirection: "row", justifyContent: "space-around" }}
+            >
+              <Pressable
+                onPress={() => addNotification(5)}
+                style={{
+                  backgroundColor: "#F58549",
+                  paddingVertical: 10,
+                  paddingHorizontal: 20,
+                  borderRadius: 10,
+                }}
+              >
+                <Text style={{ color: "#FFF", fontWeight: "500" }}>5 min</Text>
               </Pressable>
-              <Pressable onPress={() => addNotification(30)} style={{ backgroundColor: '#F58549', paddingVertical: 10, paddingHorizontal: 20, borderRadius: 10 }}>
-                <Text style={{ color: '#FFF', fontWeight: '500' }}>30 min</Text>
+              <Pressable
+                onPress={() => addNotification(30)}
+                style={{
+                  backgroundColor: "#F58549",
+                  paddingVertical: 10,
+                  paddingHorizontal: 20,
+                  borderRadius: 10,
+                }}
+              >
+                <Text style={{ color: "#FFF", fontWeight: "500" }}>30 min</Text>
               </Pressable>
-              <Pressable onPress={() => addNotification(60)} style={{ backgroundColor: '#F58549', paddingVertical: 10, paddingHorizontal: 20, borderRadius: 10 }}>
-                <Text style={{ color: '#FFF', fontWeight: '500' }}>1 h</Text>
+              <Pressable
+                onPress={() => addNotification(60)}
+                style={{
+                  backgroundColor: "#F58549",
+                  paddingVertical: 10,
+                  paddingHorizontal: 20,
+                  borderRadius: 10,
+                }}
+              >
+                <Text style={{ color: "#FFF", fontWeight: "500" }}>1 h</Text>
               </Pressable>
             </View>
           </View>
